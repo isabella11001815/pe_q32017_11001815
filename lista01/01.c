15d5c7499a7294538296a0d3c92cb5f4d81e8144
isabella11001815 <32200264+isabella11001815@users.noreply.github.com>
2017-09-29 11:43:05 -0300

    Lista 01

--- lista01/01.c
+++ lista01/01.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int main(){
-	int valor;
-	printf("Entre com um valor\n");
-	scanf("%d", &valor);
+int main ()
+{
 
-	if ((valor%2)==0) {
-		printf("Numero e par\n");
-	} else {
-		printf("Numero e impar\n");
-	}
+    int valor;
+    printf("Entre com um valor \n"); 
+    scanf("%d" , &valor); /* Criando uma variável para receber um inteiro do usuario */
+ 
+    if ( (valor%2)==0 ) {
+        printf("O numero e par\n"); /* O resto da divisao do numero por 2 e zero */
+    } else {
+        printf("O numero e impar\n"); /* Caso contrario, o numero e impar */
+    }
 
-	return 0;
+ return 0;
+ 
 }
