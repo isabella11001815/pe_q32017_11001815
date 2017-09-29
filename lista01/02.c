c28fd453ffc1ffc4dd14cd783d97b8508507cbcd
isabella11001815 <32200264+isabella11001815@users.noreply.github.com>
2017-09-29 11:24:38 -0300

    Add files via upload

---
+++ lista01/02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    int y;
+    printf("Digite o valor de x:");
+    scanf("%d",&x);
+    printf("Digite o valor de y:");
+    scanf("%d",&y); /* Recebemos os valores de x e y */
+  
+    int soma;
+    soma = x+y; /*Cria uma variável inteira que guarda a soma de x e y */
+  
+    printf("A soma de x e y é: %d", soma);
+  
+  return 0;
+  
+}
